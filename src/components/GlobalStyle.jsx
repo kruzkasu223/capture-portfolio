5f8149896917fe0f9c9843bd2b4b7fd619664f87
Krushang Kasundra <kruzkasu223@gmail.com>
2021-03-03 16:57:04 +0530

    implementing styled components and styling about section...

---
+++ src/components/GlobalStyle.jsx
@@ -0,0 +1,61 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+body{
+    background-color: #1b1b1b;
+    color: white;
+}
+
+*::selection{
+    color:#1b1b1b;
+    background-color: #23d997;
+}
+
+button{
+    font-weight: bold;
+    font-size: 1.1rem;
+    cursor: pointer;
+    padding: 1rem 2rem;
+    border: 3px solid #23d997;
+    background: transparent;
+    color: white;
+    transition: .3s all ease;
+
+    &:hover,
+    &:focus{
+        background-color: #23d997;
+        color: white;
+    }
+}
+
+h2{
+    font-weight: lighter;
+    font-size: 4rem;
+}
+
+a{
+    font-size: 1.1rem;
+}
+
+span{
+    font-weight: bold;
+    color: #23d997;
+}
+
+p{
+    padding: 3rem 0;
+    color: #ccc;
+    font-size: 1.4rem;
+    line-height: 150%;
+}
+
+`;
+
+export default GlobalStyle;
