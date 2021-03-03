0f46d5e71240fc633ccbcea7376952ff4c6f1403
Krushang Kasundra <kruzkasu223@gmail.com>
2021-03-03 16:02:21 +0530

    created services section...

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,5 +1,11 @@
 import AboutSection from "../components/AboutSection";
+import ServicesSection from "../components/ServicesSection";
 
 export default function AboutUS() {
-    return <AboutSection />;
+    return (
+        <>
+            <AboutSection />
+            <ServicesSection />
+        </>
+    );
 }
