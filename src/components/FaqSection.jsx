b3d04a2b95f424f41c9be6ae15427f0eecf8bb3a
Krushang Kasundra <kruzkasu223@gmail.com>
2021-03-03 16:10:12 +0530

    created faqs section...

---
+++ src/components/FaqSection.jsx
@@ -0,0 +1,49 @@
+export default function FaqSection() {
+    return (
+        <div className="faq">
+            <h2>
+                Any questions? <span>FAQs</span>
+            </h2>
+            <div className="question">
+                <h4>How do I start?</h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing
+                        elit. Esse, officiis?
+                    </p>
+                </div>
+            </div>
+            <div className="question">
+                <h4>Daily Schedule</h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing
+                        elit. Esse, officiis?
+                    </p>
+                </div>
+            </div>
+            <div className="question">
+                <h4>Different Payment Methods</h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing
+                        elit. Esse, officiis?
+                    </p>
+                </div>
+            </div>
+            <div className="question">
+                <h4>What products do we offer?</h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing
+                        elit. Esse, officiis?
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+}
--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,11 +1,13 @@
 import AboutSection from "../components/AboutSection";
 import ServicesSection from "../components/ServicesSection";
+import FaqSection from "../components/FaqSection";
 
 export default function AboutUS() {
     return (
         <>
             <AboutSection />
             <ServicesSection />
+            <FaqSection />
         </>
     );
 }
